eb028b1a7b0ac5d7650840f6fe37fa44710d11a9
MuhammadRanuFawwaz <ranufawaz95@gmail.com>
2023-10-06 16:16:57 +0700

    menambahkan semua file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php class Contoh1 extends
+CI_controller
+{       public function
+index()
+{   echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Muhammad Ranu Fawwaz Saya
+    tinggal di daerah Bekasi Olahraga yang saya sukai adalah 
+    Sepak bola";
+
+}    
+
+}
